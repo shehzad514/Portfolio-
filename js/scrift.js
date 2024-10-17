39f60381db6aa9aca907aa8c99248ee669cca493
Shehzad1804 <77831919+shehzad514@users.noreply.github.com>
2024-10-17 14:46:03 +0500

    Add files via upload

---
+++ js/scrift.js
@@ -0,0 +1,10 @@
+
+
+
+var typed3 = new Typed('#typing', {
+    strings: ['Apps-Developer', 'Graphic-Designer', ' Web-Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
